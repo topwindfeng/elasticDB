98c2611647624496693ce4a0cce97a2d57f0795e
topwindfeng <roger_feng@hotmai.com>
2018-05-31 22:17:37 -0400

    bittiger

---
+++ scripts/set_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o allexport
+
+MYSQL_USERNAME='root'
+MYSQL_PASSWORD='TigerBit!2016'
+
+# HOSTS
+MASTER=18.218.245.184
+SLAVE=(52.15.57.71 18.188.219.117)
+CANDIDATE=(18.188.58.208)
+
+set +o allexport
+
